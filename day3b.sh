26c1938e80bf85f038627fb5bd645aa713d865ae
Jes Wolfe <jes@scroll.com>
2020-12-03 09:16:34 -0800

    faster 3b

--- day3b.sh
+++ day3b.sh
@@ -8,10 +8,19 @@ for rule in .1 ...1 .....1 .......1 .2 ; do
 
   cat input3.txt > tmp3b
 
-  while [ -s tmp3b ] ; do
-    sed -i '' '1,'$skips'd; s/\('$dots'\)\(.*\)/\2\1/' tmp3b
-    sed -n '1s/^#.*/1/p' tmp3b
-  done | wc -l | tee -a tmp3b.counts
+  ## sensible version
+  if false ; then
+    while [ -s tmp3b ] ; do
+      sed -i '' '1,'$skips'd; s/\('$dots'\)\(.*\)/\2\1/' tmp3b
+      sed -n '1s/^#.*/1/w /dev/stdout' tmp3b
+    done | wc -l | tee -a tmp3b.counts
+  else
+  ## fast version
+    lines=`wc -l < input3.txt | xargs`
+    (eval "echo {2..$lines}"',\$'s'/\\\('$dots'\\\)\\\(.*\\\)/\\2\\1/\ \;' ; echo "/^#/p") | sed -E 's/; */\
+/g' | sed '/^$/d' > tmp3.sed
+    cat input3.txt | awk "(NR + 1) % $skips == 0" | sed -nf tmp3.sed | wc -l | tee -a tmp3b.counts
+  fi
 done
 
 (cat tmp3b.counts ; sed -n '2,$s/.*/*/p' tmp3b.counts ; echo "p") | dc
